20f9a0cd4fcf26790db7188518085affa3c335e3
Bashir Alam <57365833+readandread@users.noreply.github.com>
2021-05-17 23:53:24 +0600

    Add files via upload

---
+++ JavaFxProblems/MiniBrowser/Main.java
@@ -0,0 +1,24 @@
+package sample;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Web Browser");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
